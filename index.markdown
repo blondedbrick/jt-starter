8f063c8788a1d9a471f9636dd23f384ad4fc9563
blondedbrick <93733849+blondedbrick@users.noreply.github.com>
2024-09-21 21:10:01 -0500

    initial commit

---
+++ index.markdown
@@ -0,0 +1,16 @@
+---
+# Feel free to add content and custom Front Matter to this file.
+# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+
+layout: default
+title: Landing Page
+---
+
+# Welcome to {{ page.title }}
+{: .text-5xl}
+
+Don't worry about things you don't have control over.
+{: .mb-8}
+
+Button
+{: .btn .btn-secondary}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Include paths for Jekyll's generated HTML files
+    './_site/**/*.html',
+    './*.html',
+    './_layouts/**/*.html',
+    './_includes/**/*.html',
+    './_posts/**/*.html',
+  ],
+  theme: {
+    extend: {
+      // Extend Tailwind's default theme as needed
+      fontFamily: {
+        // Example custom fonts
+        sans: ['Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
